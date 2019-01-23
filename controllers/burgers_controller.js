746eb6d3b5940ee5c97a7ed6b9782125e1eb7484
[verna100] <vernalizg@gmail.com>
2019-01-22 22:48:29 -0500

    heroku push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,12 +2,12 @@ var express = require ("express");
 var router = express.Router()
 var burger = require ("../models/burgers.js");
 
-router.get('/', function(req,res){
-  burger.all(function(burger_data){
-    // console.log(burger_data);
-    res.render('index', {burger_data});
-   })
-})
+// router.get('/', function(req,res){
+//   burger.all(function(burger_data){
+//     // console.log(burger_data);
+//     res.render('index', {burger_data});
+//    })
+// })
 
 router.put('/burgers/update', function (req, res){ 
   burger.update(req.body.burger_id, function (result){
