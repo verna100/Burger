698d7ebf42a46cb8d1d6fc5480992088aad50063
[verna100] <vernalizg@gmail.com>
2019-01-24 21:38:59 -0500

    changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,47 +2,45 @@ var express = require ("express");
 var router = express.Router()
 var burger = require ("../models/burgers.js");
 
-router.get('/', function(req,res){
-  burger.all(function(burger_data){
-    // console.log(burger_data);
-    res.render('index', {burger_data});
-   })
-})
-// router.get("/", function(req, res) {
-//   burger.all(function(data) {
-//     var hbsObject = {
-//       burgers: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
 
-
-router.put('/burgers/update', function (req, res){ 
-  burger.update(req.body.burger_id, function (result){
-    // console.log(result);
-    res.redirect('/');
-  }); 
-  
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-// router.put("/burgers/update/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.post("/burgers/create", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
 
-//   console.log("condition", condition);
 
-//   burger.update({
-//     devoured: req.body.devoured
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+router.put("/burgers/update/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 router.post('/burgers/create', function (req, res){ 
@@ -55,16 +53,6 @@ router.post('/burgers/create', function (req, res){
 });
 
 
-// router.post("/burgers/create", function(req, res) {
-//   burger.create([
-//     "burger_name", "devoured"
-//   ], [
-//     req.body.burger_name, req.body.devoured
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
 
 
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,38 +5,39 @@ var newDevour;
       event.preventDefault();
   
         newBurger = {
-        burger_name: $("#new").val().trim()
-     
+        burger_name: $("#new").val().trim(),
+        devoured: 0
       } 
-    // $(".change-devour").on("click", function(event) {
-    //   var id = $(this).data("id");
-    //   newDevour = $(this).data("newdevour");
-  
-    //   newDevourState = {
-    //     devoured: newDevour
-    //   };
 
-    //   $.ajax("/burgers/update" + id, {
-    //     type: "PUT",
-    //     data: newDevourState
-    //   }).then(
-    //     function() {
-    //       console.log("changed devour status to", newDevour);
-    //       location.reload();
-    //     }
-    //   );
-    // });
+      $.ajax("/burgers/create", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          location.reload();
+        }
+      );
+    });
+
+    $(function(){
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      newDevour = !$(this).data("newdevour");
+  
+      newDevourState = {
+        devoured: newDevour
+      };
+
+      $.ajax("/burgers/update" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed devour status to", newDevour);
+          location.reload();
+        }
+      );
+    });
     // console.log(newBurger);
-
-    $.ajax("/burgers/create", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new Burger");
-        // location.reload();
-      }
-    );
-  });
-  
-// };
+    });
